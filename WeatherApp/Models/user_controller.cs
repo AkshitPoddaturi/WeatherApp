57480a4fd2e58f3427d4c3395596cfac692a7881
AkshitPoddaturi <akshitpoddaturi@gmail.com>
2024-09-07 00:43:09 +0530

    Add project files.

---
+++ WeatherApp/Models/user_controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApp.Models
+{
+    public class user_controller
+    {
+        public int Number { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
